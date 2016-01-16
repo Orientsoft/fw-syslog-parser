245a39c03c53cafdb3bb3954db3152da6d2e41c7
Xie Di <xiedidan@yeah.net>
2016-01-16 10:22:14 +0800

    fix pipe input

--- fw-syslog-parser.js
+++ fw-syslog-parser.js
@@ -11,18 +11,21 @@ client.on('error', function(e) {
 
 var rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: false
 })
 
-rl.on('line', function(line) {
-  // got a line, parse
-  try {
-    var json = parser.parse(line)
-    // pub result to redis
-    client.publish(config.resultChannel, JSON.stringify(json))
-  } catch(e) {
-    // pub error to redis
-    e.source = line
-    client.publish(config.resultChannel + '-error', JSON.stringify(e))
-  }
+client.on('connect', function() {
+  rl.on('line', function(line) {
+    // got a line, parse
+    try {
+      var json = parser.parse(line)
+      // pub result to redis
+      client.publish(config.resultChannel, JSON.stringify(json))
+    } catch(e) {
+      // pub error to redis
+      e.source = line
+      client.publish(config.resultChannel + '-error', JSON.stringify(e))
+    }
+  })
 })
